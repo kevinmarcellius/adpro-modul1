ab834bf7fc1b040a363afa2d16fbc7ef710264ba
kevinmarcellius <kevin.marcellius@ui.ac.id>
2024-02-01 22:23:25 +0700

    create Product.java

---
+++ src/main/java/id/ac/cs/ui/advprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.cs.ui.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
